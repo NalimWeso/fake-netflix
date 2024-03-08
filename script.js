983fcc5c964e4c2a285f34b8bbb073804ece9d1b
Milan Wesołowski <milanweso@gmail.com>
2024-03-08 17:45:58 +0100

    Upgrade thumbnails

--- script.js
+++ script.js
@@ -5,49 +5,49 @@ document.querySelector('.match-score').innerHTML = `${randomPercent}% Match`;
 const episodes = [
     {
         id: 1,
-        thumbnail: "video.jpg",
+        thumbnail: "episode-1.jpg",
         title: "1. Our Great Washing Machines",
         description: " Join us as we delve into the fascinating history and evolution of washing machines. From hand-cranked contraptions to modern marvels, discover the innovations that revolutionized laundry day."
     },
     {
         id: 2,
-        thumbnail: "video.jpg",
+        thumbnail: "episode-2.jpg",
         title: "2. Wash Day Wonders",
         description: "Explore the daily rituals and challenges of laundry day across different cultures and time periods. From ancient methods to cutting-edge technology, witness the enduring significance of this household chore."
     },
     {
         id: 3,
-        thumbnail: "video.jpg",
-        title: "3. Breaking the Mold",
-        description: "Uncover the science behind washing machines and their role in maintaining cleanliness and hygiene. Learn about the battle against mold and mildew, and how modern innovations are tackling this problem."
-    },
-    {
-        id: 4,
-        thumbnail: "video.jpg",
-        title: "4. Behind Closed Doors",
-        description: "Journey behind the scenes of the laundry room to uncover the secrets and surprises lurking within. From lost socks to unexpected discoveries, witness the drama and humor of everyday laundry life."
-    },
-    {
-        id: 5,
-        thumbnail: "video.jpg",
-        title: "5. The Famous Spin Cycle",
-        description: "Step into the whirlwind world of the spin cycle, where clothes are tossed and tumbled to perfection. Explore the mechanics and magic behind this iconic stage of the washing process."
-    },
-    {
-        id: 6,
-        thumbnail: "video.jpg",
-        title: "6. From Scrubbing to Spinning",
+        thumbnail: "episode-3.jpg",
+        title: "3. From Scrubbing to Spinning",
         description: "Trace the evolution of laundry techniques from manual scrubbing to automated spinning. Follow the journey of labor-saving devices and their impact on households throughout history."
     },
     {
+        id: 4,
+        thumbnail: "episode-4.jpg",
+        title: "4. Breaking the Mold",
+        description: "Uncover the science behind washing machines and their role in maintaining cleanliness and hygiene. Learn about the battle against mold and mildew, and how modern innovations are tackling this problem."
+    },
+    {
+        id: 5,
+        thumbnail: "episode-5.jpg",
+        title: "5. Behind Closed Doors",
+        description: "Journey behind the scenes of the laundry room to uncover the secrets and surprises lurking within. From lost socks to unexpected discoveries, witness the drama and humor of everyday laundry life."
+    },
+    {
+        id: 6,
+        thumbnail: "episode-6.jpg",
+        title: "6. The Famous Spin Cycle",
+        description: "Step into the whirlwind world of the spin cycle, where clothes are tossed and tumbled to perfection. Explore the mechanics and magic behind this iconic stage of the washing process."
+    },
+    {
         id: 7,
-        thumbnail: "video.jpg",
+        thumbnail: "episode-7.jpg",
         title: "7. Rinse, Spin, Refresh",
         description: "Experience the satisfaction of a freshly laundered load as we delve into the final stages of the washing process. Discover the art of rinsing, the precision of spinning, and the revitalizing refreshment."
     },
     {
         id: 8,
-        thumbnail: "video.jpg",
+        thumbnail: "episode-8.jpg",
         title: "8. Wash and Learn",
         description: "Conclude our journey with valuable gained from the world of washing machines. From sustainability tips to laundry hacks, empower yourself with knowledge to conquer laundry day with ease."
     }
@@ -59,7 +59,7 @@ episodes.forEach((episode) => {
     episodesHTML += `
     <div class="episode" id="${episode.id}">
         <div class="episode-img">
-            <img src="/photos/${episode.thumbnail}" alt="Episode thumbnail" draggable=false>
+            <img src="photos/${episode.thumbnail}" alt="Thumbnail" draggable=false>
             <div class="play-episode-button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24">
                     <path
