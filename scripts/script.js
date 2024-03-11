f2aa51b21ed80574eceb41f3056d6a98ad386889
Milan Wesołowski <milanweso@gmail.com>
2024-03-11 21:58:24 +0100

    Import episodes

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,8 @@
+import { episodes } from './episodes.js';
+
 const randomPercent = Math.floor(Math.random() * (100 - 80 + 1)) + 80;
 
 document.querySelector('.match-score').innerHTML = `${randomPercent}% Match`;
-
-const episodes = [
-    {
-        id: 1,
-        thumbnail: "episode-1.jpg",
-        title: "1. Our Great Washing Machines",
-        description: "Join us as we delve into the fascinating history and evolution of washing machines. From hand-cranked contraptions to modern marvels, discover the innovations that revolutionized laundry day."
-    },
-    {
-        id: 2,
-        thumbnail: "episode-2.jpg",
-        title: "2. Wash Day Wonders",
-        description: "Explore the daily rituals and challenges of laundry day across different cultures and time periods. Out of ancient methods to cutting-edge technology, witness the enduring significance of this household chore."
-    },
-    {
-        id: 3,
-        thumbnail: "episode-3.jpg",
-        title: "3. From Scrubbing to Spinning",
-        description: "Trace the evolution of laundry techniques from manual scrubbing to automated spinning. Follow the journey of labor-saving devices and their impact on households throughout history."
-    },
-    {
-        id: 4,
-        thumbnail: "episode-4.jpg",
-        title: "4. Breaking the Mold",
-        description: "Uncover the science behind washing machines and their role in maintaining cleanliness and hygiene. Learn about the battle against mold and mildew, and how modern innovations are tackling this problem."
-    },
-    {
-        id: 5,
-        thumbnail: "episode-5.jpg",
-        title: "5. Behind Closed Doors",
-        description: "Journey behind the scenes of the laundry room to uncover the secrets and surprises lurking within. From lost socks to unexpected discoveries, witness the drama and humor of everyday laundry life."
-    },
-    {
-        id: 6,
-        thumbnail: "episode-6.jpg",
-        title: "6. The Famous Spin Cycle",
-        description: "Step into the whirlwind world of the spin cycle, where clothes are tossed and tumbled to perfection. Explore the mechanics and magic behind this iconic stage of the washing process."
-    },
-    {
-        id: 7,
-        thumbnail: "episode-7.jpg",
-        title: "7. Rinse, Spin, Refresh",
-        description: "Experience the satisfaction of a freshly laundered load as we delve into the final stages of the washing process. Discover the art of rinsing, the precision of spinning, and the revitalizing refreshment."
-    },
-    {
-        id: 8,
-        thumbnail: "episode-8.jpg",
-        title: "8. Wash and Learn",
-        description: "Conclude our journey with valuable gained from the world of washing machines. From sustainability tips to laundry hacks, empower yourself with knowledge to conquer laundry day with ease."
-    }
-];
 
 let episodesHTML = '';
 
